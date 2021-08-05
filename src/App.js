aa704db827568bb69a1ea3e04929cee2370a197d
yosri abdelli <yosriabdelli1@gmail.com>
2021-08-05 01:47:46 +0100

    link section

--- src/App.js
+++ src/App.js
@@ -3,36 +3,52 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Particles from 'react-particles-js';
 import Navbar from './components/Navbar';
 import Header from './components/Header';
+
+import {
+  ScrollingProvider,
+  useScrollSection,
+  Section,
+} from 'react-scroll-section';
+
+
 function App() {
   return (
     <>
-      <Particles
-      className="particles-canvas"
-      params={{
-        particles:{
-          Number:{
-            value :30,
-            desity :{
-              enable : true,
-              value_area : 900
-            }
-          },
-          "size":{
-            value:2
-          },
-          shape:{
-            type:"circle",
-            stroke: {
-              width:5,
-              color:"#f9ab00"
-            }
-          }
-        }
-        
-      }}
-      />
-      <Navbar/>
-      <Header/>
+      <ScrollingProvider>
+        <Section id="home">
+          <Navbar/>
+          <Header/>
+          <Particles
+            className="particles-canvas"
+            params={{
+              particles:{
+                Number:{
+                  value :30,
+                  desity :{
+                    enable : true,
+                    value_area : 900
+                  }
+                },
+                "size":{
+                  value:2
+                },
+                shape:{
+                  type:"circle",
+                  stroke: {
+                    width:5,
+                    color:"#f9ab00"
+                  }
+                }
+              }
+              
+            }}
+          />
+        </Section>
+        <Section id="about" className="box">ABOUT ME</Section>
+        <Section id="services" className="box">Services</Section>
+        <Section id="projets" className="box">Projets</Section>
+        <Section id="contacts"className="box">Contacts</Section>
+      </ScrollingProvider>
     </>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,11 +2,21 @@ import React from "react"
 //React Fontawsome React
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
-
-
+import {
+    ScrollingProvider,
+    useScrollSection,
+    Section,
+  } from 'react-scroll-section';
+    
 const Navbar = () => {
+    const homeSection = useScrollSection('home');
+    const aboutSection = useScrollSection('about');
+    const servicesSection =useScrollSection('services');
+    const projetsSection =useScrollSection('projets');
+    const contactsSection =useScrollSection('contacts');
+
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-dark py-4">
+        <nav className="navbar navbar-expand-lg navbar-light bg-dark py-4 fixed-top">
             <div className="container">
                 <a className="navbar-brand" href="#">Portfolio</a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -14,20 +24,20 @@ const Navbar = () => {
                 </button>
                 <div className="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
                     <ul className="navbar-nav ml-auto">
-                        <li className="nav-item active">
-                            <a className="nav-link" href="#">Accueil</a>
+                        <li className="nav-item active" onClick={homeSection.onClick} selected={homeSection.selected}>
+                            <span className="nav-link">Accueil</span>
                         </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">A propos</a>
+                        <li className="nav-item" onClick={aboutSection.onClick} selected={aboutSection.selected}>
+                            <span className="nav-link" >A propos</span>
                         </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">Services</a>
+                        <li className="nav-item" onClick={servicesSection.onClick} selected={servicesSection.selected}>
+                            <span className="nav-link" >Services</span>
                         </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">Projets</a>
+                        <li className="nav-item" onClick={projetsSection.onClick} selected={projetsSection.selected}>
+                            <span className="nav-link" >Projets</span>
                         </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">Contacts</a>
+                        <li className="nav-item" onClick={contactsSection.onClick} selected={contactsSection.selected}>
+                            <span className="nav-link">Contacts</span>
                         </li>
                     </ul>
                 </div>
